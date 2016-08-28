e72054d27f41acaebe5e1dcd85b9206588ea970b
Andrew Paterson <andrew.ian.paterson@gmail.com>
2016-08-28 17:31:37 +0200

    Optimise Tree bounding calculations.

---
+++ src/net/engine/shape/result/DistanceResult.java
@@ -0,0 +1,13 @@
+package net.engine.shape.result;
+
+public class DistanceResult
+{
+  public float cross;
+  public float dot;
+
+  public DistanceResult(float cross, float dot)
+  {
+    this.cross = cross;
+    this.dot = dot;
+  }
+}
